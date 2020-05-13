7344b18ddd336dbafdd0706fcff94a449ddf8af6
Brian LeRoux <b@brian.io>
2020-05-13 11:05:52 -0700

    Update index.ts

--- hello-world/index.ts
+++ hello-world/index.ts
@@ -1,7 +1,5 @@
-import { Context, Event } from "https://deno.land/x/lambda/mod.ts"
-
-export async function handler(event: Event, context: Context) {
-  console.log('hello from Deno', event, context)
+export async function handler(req: object) {
+  console.log('hello from Deno', req)
   return {
     statusCode: 200,
     headers: {'content-type': 'text/html;charset=utf8'},
